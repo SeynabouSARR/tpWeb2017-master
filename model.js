f6ba1a3b0ea00092917c8f88550f56c4300601d4
Seynabou SARR <seynabousarr90@yahoo.fr>
2018-01-27 19:51:56 +0100

    modif

--- model.js
+++ model.js
@@ -7,23 +7,25 @@ function Drawing(){
 
 }
 
-function Rectangle(haut-gauche, largeur, hauteur){
-        var haut-gauche;
-        var largeur;
-        var hauteur;
+function Rectangle(haut_gauche, largeur, hauteur){
+       Drawing.call(this,haut_gauche,largeur,hauteur);
+        this.haut_gauche=haut_gauche;
+        this.largeur=largeur;
+        this.hauteur=hauteur;
 
 }
 
 function Line(x1, x2, y1, y2){
-        var x1;
-        var x2;
-        var y1;
-        var y2;
+    Drawing.call(this,x1,x2,y1,y2);
+        this.x1=x1;
+        this.x2=x2;
+        this.y1=y1;
+        this.y2=y2;
 
 }
 
-function Form(couleur, epaisseur;){
-         var couleur;
-         var epaisseur;
+function Form(couleur, epaisseur){
+         this.couleur=couleur;
+         this.epaisseur=couleur;
 
 }
